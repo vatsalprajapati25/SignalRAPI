281b0f2ba9c9a629ee07aa5fe1ed9691ddf1ca38
SIT328 <SIT328@SITPC0328>
2024-01-24 17:39:13 +0530

    Remove unused imports.

--- API/Controllers/ChatController.cs
+++ API/Controllers/ChatController.cs
@@ -1,6 +1,5 @@
 ﻿using API.Models;
 using API.Services;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,12 @@
 using API.Hubs;
 using API.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSingleton<ChatService>();
