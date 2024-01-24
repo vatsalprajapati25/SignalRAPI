76568ead64ae295580612ad65c5e0b41945b68b7
SIT328 <SIT328@SITPC0328>
2024-01-24 16:46:06 +0530

    Added personal chat

--- API/Hubs/ChatHub.cs
+++ API/Hubs/ChatHub.cs
@@ -47,5 +47,36 @@ namespace API.Hubs
         {
             await Clients.Group("WebChat").SendAsync("NewMessage", message);
         }
+
+        public async Task Chat(MessageModel message)
+        {
+            string privateGroupName = GetPrivateGroupName(message.From, message.To);
+            await Groups.AddToGroupAsync(Context.ConnectionId, privateGroupName);
+            var toConnection =  _chatService.GetConnectionIdByUser(message.To);
+            await Groups.AddToGroupAsync(toConnection, privateGroupName);
+            await Clients.Client(toConnection).SendAsync("OpenPrivateChat", message);
+        }
+
+        public async Task ReceivePrivateMessages(MessageModel messages)
+        {
+            string privateGroupName = GetPrivateGroupName(messages.From, messages.To);
+            await Clients.Group(privateGroupName).SendAsync("NewPrivateMessage", messages);
+        }
+
+        public async Task RemovePrivateChat(string from, string to)
+        {
+            string privateGroupName = GetPrivateGroupName(from, to);
+            await Clients.Group(privateGroupName).SendAsync("ClosePrivateChat");
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, privateGroupName);
+            var toConnId = _chatService.GetConnectionIdByUser(Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(toConnId, privateGroupName);
+        }
+
+
+        public static string GetPrivateGroupName(string from, string to)
+        {
+            var stringCompare = string.CompareOrdinal(from, to) < 0;
+            return stringCompare ? $"{from}-{to}" : $"{to}-{from}";
+        }
     }
 }
